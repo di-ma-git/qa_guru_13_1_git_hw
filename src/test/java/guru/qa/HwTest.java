a0b25b037b76dbc14b3aa09d0d4c9aa938568883
Dima Maltsev <phoenixnety@gmail.com>
2022-07-05 22:05:22 +0300

    first commit

---
+++ src/test/java/guru/qa/HwTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HwTest {
+
+    @Test
+    void myHwGitTest() {
+
+        Assertions.assertTrue(33 != 5);
+
+    }
+}
